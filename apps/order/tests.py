bde13582ccbecdf39ca371cd3f706f416608767b
Suxrob <suxidev@gmail.com>
2023-08-26 16:31:24 +0500

    added test

--- apps/order/tests.py
+++ apps/order/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Order,Pet
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def OrderTest(self):
+        self.petid = Pet.objects.create(
+            id = self,
+
+        ) 
+        self.user = Order.objects.create(
+            name="DogSale",
+            petId="",
+            quantity="Pass12345",
+            shipDate="Javohir",
+            status="Rustamov",
+            complete="Fals"
+        )
+
+    def test_user(self):
+        self.assertEqual(self.user.username, "test")
+        self.assertEqual(self.user.email, "test@gmail.com")
+        self.assertEqual(self.user.password, "Pass12345")
+        self.assertEqual(self.user.firstName, "Javohir")
+        self.assertEqual(self.user.lastName, "Rustamov")
+

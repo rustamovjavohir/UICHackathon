84b9b45506b5455f8d89eb0cbe545b58cbe6508e
Suxrob <suxidev@gmail.com>
2023-08-26 15:51:30 +0500

    added order model

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from utils.models import BaseModel, SlugModel
+from product.models import Pet
 # Create your models here.
+class Order(SlugModel):
+    name = models.CharField(max_length=255, verbose_name='Название тега')
+    STATUS = (
+        ('placed','placed'),
+        ('approved',' approved'),
+        ('delivered','delivered'),
+    )
+    petId = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=1000, decimal_places=2)
+    shipDate = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length = 20, choices = STATUS, default = 'available')
+    complete = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name[:10] or self.name
